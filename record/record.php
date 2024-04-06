07bd28fc215cd30ccc47186415c3cdd7785eed26
Vaughn Gugger <vjgtigers@gmail.com>
2024-04-05 22:36:40 -0400

    removed unused and changed currently used file

--- record/record.php
+++ record/record.php
@@ -1,17 +1,29 @@
-
-
-
-
-
+<!DOCTYPE html>
+<html>
 
 
 
 <?php
 
-echo 'version2';
+if(isset($_COOKIE['XERWAILOGIN'])) {
+  //echo json_encode("NOT_LOGGED_IN");
+  //die;
+} else {
+  echo json_encode("NOT_LOGGED_IN");
+  die;
+
+}
+
+
+
+
+
+echo 'version4<br>';
 echo htmlspecialchars($_POST["date"]);
 echo '<br>';
 $w = htmlspecialchars($_POST["weekend"]);
+$pay = htmlspecialchars($_POST["pay"]);
+
 echo '<br>';
 if ($w == "") {
     echo "not weekend";
@@ -29,37 +41,58 @@ if ($w == "") {
 echo '<br>';
 echo htmlspecialchars($_POST["hours"]);
 echo '<br>';
-echo htmlspecialchars($_POST["userid"]);
+echo 'If this page doesnt redirect something went horibbly wrong, please close the tab and try again.';
+
 echo '<br>';
 
 
+$startTime = time();
+$dttime = date("Y-m-d H:i:s",$startTime);
+$fname = "recordPage";
+include '../../config.php';
 
+include '../recordTime.php';
 
+//db connection
 
-
-include '../../config.php';//db connection
-
-//weekend set up top
+//weekend set up top + hourly pay
 $date = htmlspecialchars($_POST["date"]);
 $hours = htmlspecialchars($_POST["hours"]);
-$userid = htmlspecialchars($_POST["userid"]);
 //fetch data
+
+
+
+
+
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
+  recordUse($fname, $dttime, $startTime, time(), $userID, "connFail");
   die("Connection failed: " . $conn->connect_error);
 }
 
 
-$sql = "INSERT INTO past_payment_data (date_info, hours, weekend, user_id)
-VALUES ('$date', $hours, '$weekend', '$userid')";
+
+$result = $conn->execute_query('SELECT search_id FROM locationtracking.cookies_table WHERE cookie = ?', [$_COOKIE['XERWAILOGIN']]);
+ while ($row = $result->fetch_assoc()) {
+  $userID = $row["search_id"];
+ }
+
+
+
+
+
+$sql = "INSERT INTO past_payment_data (date_info, hours, weekend, user_id, hourly_pay)
+VALUES ('$date', '$hours', '$weekend', '$userID', '$pay')";
 
 
 
 if ($conn->query($sql) === TRUE) {
+  recordUse($fname, $dttime, $startTime, time(), $userID, "recordSuccess");
   echo "New record created successfully";
 } else {
+  recordUse($fname, $dttime, $startTime, time(), $userID, "recordFail");
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
@@ -67,10 +100,21 @@ echo "45";
 
 
 ?>
-echo '<script type="text/JavaScript">
-    location.replace("https://xerwai.com/paytracking/info");
-    </script>'
-;
+
+<form id="myForm" action="../info/shortdata.php" method="post">
+<?php
+        echo '<input type="hidden" name="'.'userid'.'" value="'.$userid.'">';
+?>
+</form>
+<script type="text/javascript">
+    document.getElementById('myForm').submit();
+</script>
+
+<!--
+<script type="text/JavaScript">
+    location.replace("https://xerwai.com/paytracking/info/shortdata.php");
+</script>
+-->
 
 
 
