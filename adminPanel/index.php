51406c2bb4fc56228088bb1cc8bb0b8d8fec4074
Vaughn Gugger <vjgtigers@gmail.com>
2024-04-08 22:28:49 -0400

    added begining of admin page. no functionality yet

---
+++ adminPanel/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./style.css">
+  </head>
+  <body>
+	<script src="index.js"></script>
+
+<!-- nav bar leaves a little space on the left -->
+<div id="navBar" class="red_body">
+ <p class="centerme PLarge TBMZ">Admin Panel</p>
+</div>
+
+
+
+
+
+  </body>
+</html>
